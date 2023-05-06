757f26e7bf74c2d62c934bd1a58595b38ff8175d
TFMRO <mero@gtcraft.eu>
2023-05-06 21:52:55 +0200

    Added something idk

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import json
 import requests
 import os
 import sys
-if sys.platform == 'win32':
-    os.system('mv scripts\\Windows\\install.bat ..\\')
-    os.system('mv scripts\\Windows\\Start.bat ..\\')
-elif not system.platform == 'win32':
-    os.system('mv scripts\\Linux\\install.sh ..\\')
-    os.system('mv scripts\\Linux\\run.sh ..\\')
+import webbrowser
 if sys.platform == 'win32':
     os.system("title " + "VPN Check tool - Github.com/Realzuko")
 
 
-# Load the API key from config.json file
-with open('config.json') as j:
-    config = json.load(j)
-    api_key = config.get('api_key', '')
+# Load the API key from api.json file
+with open('api.json') as j:
+    api = json.load(j)
+    api_key = api.get('api_key', '')
 
-# If the API key is empty or set to "PLACE_API_KEY_HERE", ask for the API key and save it to config.json file
+# If the API key is empty or set to "PLACE_API_KEY_HERE", ask for the API key and save it to api.json file
 if not api_key or api_key == "ENTER_API_KEY_HERE":
     api_key = input("Enter API key: ")
-    with open('config.json', 'w') as f:
+    with open('api.json', 'w') as f:
         json.dump({'api_key': api_key}, f)
 
 
@@ -55,6 +50,7 @@ print(f"IP Given: {ip_given}")
 print(f"VPN/Proxy: {vpn_proxy}")
 print(f"ISP: {isp}")
 print(f"More specific data was saved to {filename}")
+os.system("cd Results && notepad " + ip_given + ".json")
 if sys.platform == 'win32':
     os.system('color f')
 elif not sys.platform == 'win32':
